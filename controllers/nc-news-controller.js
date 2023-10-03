e68d799cba0cd06606981317aed020ddd2e4db0e
Wayne Senior <wayne_senior@hotmail.com>
2023-10-03 16:27:03 +0100

    add parametric endpoint testing and viable error handling

--- controllers/nc-news-controller.js
+++ controllers/nc-news-controller.js
@@ -1,13 +1,23 @@
-const {selectTopics} = require('../models/topics-model') 
+const { selectTopics, selectArticleById } = require("../models/topics-model");
 const endpoints = require("../endpoints.json");
 
-
 exports.getTopics = (req, res) => {
-selectTopics().then((topics) => {
-    res.status(200).send({topics})
-})
-}
+  selectTopics().then((topics) => {
+    res.status(200).send({ topics });
+  });
+};
 
 exports.getApi = (req, res) => {
-    res.status(200).send({endpoints})
-}
+  res.status(200).send({ endpoints });
+};
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
--- models/topics-model.js
+++ models/topics-model.js
@@ -5,3 +5,17 @@ exports.selectTopics = () => {
     return result.rows;
   });
 };
+
+exports.selectArticleById = (article_id) => {
+  return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+  .then(({rows}) => {
+    const article = rows[0];
+    if(!article) {
+      return Promise.reject({
+        status: 404,
+        msg: 'article does not exist'
+      })
+    }
+    return article
+  });
+};
